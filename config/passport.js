c2dcad16426aaecae2c78e3dab7d98cd4600b5b0
marcusis6 <116612133+marcusis6@users.noreply.github.com>
2022-12-07 03:25:33 -0500

    Add files via upload

---
+++ config/passport.js
@@ -0,0 +1,62 @@
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const { getAll, Append } = require("../crud");
+
+module.exports = function (passport) {
+  passport.use(
+    new LocalStrategy({ usernameField: "name" }, (name, password, done) => {
+      let username = name.toLowerCase();
+
+      // Match User
+      (async function () {
+        let users;
+        users = getAll("./storage/users.json");
+        if (users.length > 0) {
+          users = users.filter((x) => x.username == name);
+          return users[0];
+        }
+      })()
+        .then((user) => {
+          if (!user) {
+            return done(null, false, { message: "এই নামে কোন আইডি নেই" });
+          }
+
+          if (!user.isApproved) {
+            return done(null, false, { message: "এই আইডি এখনও অনুমোদিত হয়নি" });
+          }
+
+          // Match password
+          bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err) throw err;
+
+            if (isMatch) {
+              return done(null, user);
+            } else {
+              return done(null, false, { message: "পাসওয়ার্ড ভুল হচ্ছে!" });
+            }
+          });
+        })
+        .catch((err) => console.log(err));
+    })
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    (async function () {
+      let users;
+      users = getAll("./storage/users.json");
+      if (users.length > 0) {
+        users = users.filter((x) => x._id == id);
+        return users[0];
+      }
+    })().then((user) => {
+      if (user) {
+        done(null, user);
+      }
+    });
+  });
+};
