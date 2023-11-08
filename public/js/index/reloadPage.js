f3d1d37ba2822f7a0e0cdcd3fb13a172d82fbcaa
spark <clock301@protonmail.com>
2023-11-07 23:22:06 -0500

    ewfewf

---
+++ public/js/index/reloadPage.js
@@ -0,0 +1,17 @@
+function startCountdown(minutes) {
+    const countdownElement = document.getElementById('countdown');
+    countdownElement.innerText = minutes;
+    
+    const countdownInterval = setInterval(() => {
+        minutes--;
+        countdownElement.innerText = minutes;
+        
+        if (minutes === 0) {
+            clearInterval(countdownInterval);
+            location.reload();
+        }
+    }, 60000); // 1 minute = 60,000 milliseconds
+}
+
+// Set the initial countdown time (30 minutes = 30)
+startCountdown(29);
